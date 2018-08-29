53ab97886139120f01e36c73f64436b9d2912373
seojaeyeon <seojaeyeon@192.168.0.73>
2018-08-29 23:50:30 +0900

    Refactoring "AccountDAO, AccountService"

--- fitnessCenterManagementSystem/src/main/java/kr/ac/fcm/dao/AccountDAO.java
+++ fitnessCenterManagementSystem/src/main/java/kr/ac/fcm/dao/AccountDAO.java
@@ -1,37 +1,42 @@
-package kr.ac.fcm.DTO.user;
+package kr.ac.fcm.dao;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.ac.fcm.DTO.user.Account;
 import kr.ac.fcm.mapper.AccountMapper;
 
 import lombok.Getter;
 
+/*
+ * Create Account and Account's Authority - save, saveAutority
+ * Read Account and Accounts' Authorities - findById, findAuthoritiesById
+ */
 @Repository
 @Getter
-public class AccountRepository {
+public class AccountDAO {
 	
 	@Autowired
 	AccountMapper accountMapper;
 	
-	public Account saveAccount(Account account){
+	public Account save(Account account){
+		accountMapper.insertUser(account);
 		return account;
 	}
 	
-	public Account save(Account account,String role){
-		accountMapper.insertUser(account);
+	public Account saveAutority(Account account, String role){
 		accountMapper.insertUserAutority(account.getId(), role);
 		return account;
 	}
 
 	public Account findById(String username) {
-		// TODO Auto-generated method stub
 		return accountMapper.readAccount(username);
 	}
 	
 	public List<String> findAuthoritiesByID(String username){
 		return accountMapper.readAutorities(username);
 	}
+	
 }
